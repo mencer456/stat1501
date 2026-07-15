44acc4448a417bf0ab4d02cfb5f25c2abe81b40a
mencer456 <hannahmencer@hotmail.com>
2026-07-14 22:28:37 -0400

    Added transformations

--- projectCode.R
+++ projectCode.R
@@ -1,49 +1,35 @@
+#reading in the data
 lending=read.csv("lending.csv", header=TRUE)
+only18=lending[grepl("18",lending$issue_d),] #keep only records from 2018
+df=only18  %>% select(dti_n, Default)         #keep only dti_n and default columns
+rownames(df)=NULL
 
-#filtering to only keep 2018 data
-df=lending[grepl("18",lending$issue_d),]
 head(df)
-View(df)
+
+#removing erroroneous values
+df$dti_n[df$dti_n>998]=NA
+df=df%>%filter(!is.na(dti_n))
+
+#categorizing default
+df$Default=factor(df$Default, levels=c(0,1),
+                  labels=c("No Default","Default"))
 
 
-#value counts of purpose
-table(df$purpose)
+# full=read.csv("lending_full.csv",header=TRUE)
+# full_only18=full[grepl("18",full$issue_d),]
+# View(full_only18)
 
-
+df$logdti=log1p(df$dti_n)
 
 library(ggplot2)
 ggplot(
-  cars,
-  aes(y = Cmb.MPG, x = Fuel) ) +
+  df,
+  aes(y = logdti, x = Default) ) +
   geom_boxplot( binaxis='y', stackdir='center')
 
 library(dplyr)
 library(stringr)
 
-df <- cars %>%
-  mutate(
-    combined_mpg_clean = if_else(
-      str_detect(Cmb.MPG, "/"),
-      {
-        parts <- str_split(Cmb.MPG, "/", simplify = TRUE)
-        (as.numeric(parts[,1]) + as.numeric(parts[,2])) / 2
-      },
-      as.numeric(Cmb.MPG)
-    )
-  )
-View(df)
+ggplot(df, aes(x=dti_n))+geom_histogram()
+ggplot(df, aes(x=logdti))+geom_histogram()
 
-
-library(ggplot2)
-ggplot(
-  df,
-  aes(y = combined_mpg_clean, x = Fuel) ) +
-  geom_boxplot( binaxis='y', stackdir='center')+
-  labs(
-    title="Combined MPG across Fuel Types",
-    x="Fuel Type",
-    y="Combined MPG"
-  )+
-  theme(
-    plot.title = element_text(hjust = 0.5, size = 16, face = "bold")
-  )
